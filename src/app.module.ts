9d6234f90a414d38077ac5e441d5027ccbd821fd
Murilo Alves Pereira Varoto <66957888+Muriloapv@users.noreply.github.com>
2025-09-17 20:14:45 -0300

    Setup backend default

---
+++ src/app.module.ts
@@ -0,0 +1,30 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config'
+import { ProdutoModule } from './produto/produto.module';
+import { UsersModule } from './users/users.module';
+@Module({
+  imports: [ 
+  ConfigModule.forRoot({
+    envFilePath: '.env', 
+    isGlobal: true  
+  }),
+  TypeOrmModule.forRoot({
+      type: "postgres",
+      host: process.env.DB_HOST,
+      // port: Number(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      entities: [  ],//adicionar manualmente as entities
+      migrations: [__dirname + '/database/migrations/*{.js,.ts}'],
+      synchronize: true,//desabilita quando for para produção
+      logging: ['query', 'error', 'schema'], 
+      autoLoadEntities: true,
+  }),
+  ProdutoModule, UsersModule],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
+
---
+++ src/main.ts
@@ -0,0 +1,29 @@
+// main.ts
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle( 'Luvr - Gestão Inteligente' )
+    .setDescription( 'Escola de T.I - software de gestão de motel.')
+    .setVersion('1.0')
+    .addTag('api')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true,
+    forbidNonWhitelisted: true,
+    transform: true,
+    transformOptions: { enableImplicitConversion: true },
+  }));
+
+  app.enableCors();
+  await app.listen( process.env.PORT ?? 3000 );
+}
+bootstrap();
