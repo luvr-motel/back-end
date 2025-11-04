c1639a75f29b6dec6e152006ad3718f3ab951d4c
Muriloapv <muriloalves121@outlook.com>
2025-11-04 10:35:53 -0300

    develop_deploy | data source import error

--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -1,83 +1,13 @@
-// import 'reflect-metadata';
-// import { DataSource } from 'typeorm';
-
-// const isProd = process.env.NODE_ENV === 'production';
-// const hasUrl = !!process.env.DATABASE_URL;
-
-// const baseOptions = {
-//   type: 'postgres' as const,
-//   entities: [__dirname + '/../**/*.entity{.js,.ts}'],
-//   migrations: [__dirname + '/migrations/*{.js,.ts}'],
-//   logging: false,
-// };
-
-// export const AppDataSource = new DataSource(
-//   hasUrl
-//     ? {
-//         ...baseOptions,
-//         url: process.env.DATABASE_URL,
-//         ssl: isProd ? { rejectUnauthorized: false } : false,
-//         synchronize: false,
-//       }
-//     : {
-//         ...baseOptions,
-//         host: process.env.DB_HOST,
-//         port: Number(process.env.DB_PORT),
-//         username: process.env.DB_USERNAME,
-//         password: process.env.DB_PASSWORD,
-//         database: process.env.DB_DATABASE,
-//         synchronize: false,
-//       }
-// );
-
-// export default AppDataSource;
-
-// src/database/data-source.ts
-// import 'reflect-metadata';
-// import { DataSource } from 'typeorm';
-
-// const isProd = process.env.NODE_ENV === 'production';
-// const hasUrl = !!process.env.DATABASE_URL;
-
-// export const AppDataSource = new DataSource(
-//   hasUrl
-//     ? {
-//         type: 'postgres',
-//         url: process.env.DATABASE_URL,
-//         ssl: isProd ? { rejectUnauthorized: false } : false,
-//         synchronize: false,
-//         entities: [__dirname + '/../**/*.entity{.ts,.js}'],
-//         migrations: [__dirname + '/migrations/*{.ts,.js}'],
-//         logging: false,
-//       }
-//     : {
-//         type: 'postgres',
-//         host: process.env.DB_HOST,
-//         port: Number(process.env.DB_PORT),
-//         username: process.env.DB_USERNAME,
-//         password: process.env.DB_PASSWORD,
-//         database: process.env.DB_DATABASE,
-//         synchronize: false,
-//         entities: [__dirname + '/../**/*.entity{.ts,.js}'],
-//         migrations: [__dirname + '/migrations/*{.ts,.js}'],
-//         logging: false,
-//       }
-// );
-
-// export default AppDataSource;
-
-
 import 'reflect-metadata';
 import { DataSource } from 'typeorm';
 import * as dotenv from 'dotenv';
 
-// Carrega variáveis do .env em ambiente local
 dotenv.config();
 
 const isProd = process.env.NODE_ENV === 'production';
 const hasDatabaseUrl = !!process.env.DATABASE_URL;
 
-export const AppDataSource = new DataSource(
+const AppDataSource = new DataSource(
   hasDatabaseUrl
     ? {
         type: 'postgres',
