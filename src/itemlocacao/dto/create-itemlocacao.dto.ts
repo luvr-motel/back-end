7580fb064b62c4bdd1a69de8ce7f779c199a36d3
Murilo Alves Pereira Varoto <66957888+Muriloapv@users.noreply.github.com>
2025-11-17 18:02:20 -0300

    s

--- src/itemlocacao/dto/create-itemlocacao.dto.ts
+++ src/itemlocacao/dto/create-itemlocacao.dto.ts
@@ -1,28 +1,33 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsInt, IsNumber, IsOptional } from 'class-validator';
 
 export class CreateItemlocacaoDto {
   @ApiProperty({
     example: 12,
     description: 'ID da locação (checkin)',
   })
+  @IsInt()
   locacao_id: number;
 
   @ApiProperty({
     example: 3,
     description: 'ID do produto consumido',
   })
+  @IsInt()
   produto_id: number;
 
   @ApiProperty({
     example: 2,
     description: 'Quantidade do item',
   })
+  @IsInt()
   qtde: number;
 
   @ApiProperty({
-    example: 15.90,
+    example: 15.9,
     description: 'Valor do item',
   })
+  @IsNumber()
   valor: number;
 
   @ApiProperty({
@@ -30,5 +35,8 @@ export class CreateItemlocacaoDto {
     description: 'ID da comanda, caso já exista. Caso null, cria nova.',
     required: false,
   })
+  @IsOptional()
+  @IsInt()
   comanda_id?: number;
 }
+*** End Patch ***!
