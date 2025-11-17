2b52e5509e75cf98265a97a04bcf163e8867bdeb
Murilo Alves Pereira Varoto <66957888+Muriloapv@users.noreply.github.com>
2025-11-17 19:36:28 -0300

    update lcoacoa

--- src/itemlocacao/itemlocacao.controller.ts
+++ src/itemlocacao/itemlocacao.controller.ts
@@ -10,8 +10,8 @@ export class ItemlocacaoController {
   constructor(private readonly itemlocacaoService: ItemlocacaoService) {}
 
   @Post()
-  @ApiOperation({ summary: 'Criar item na comanda' })
-  @ApiResponse({ status: 201, description: 'Item adicionado com sucesso' })
+  @ApiOperation({ summary: 'Criar item de locação' })
+  @ApiResponse({ status: 201, description: 'Item de locação adicionado com sucesso' })
   create(@Body() createItemlocacaoDto: CreateItemlocacaoDto) {
     return this.itemlocacaoService.create(createItemlocacaoDto);
   }
@@ -19,7 +19,7 @@ export class ItemlocacaoController {
   @Get()
   @ApiOperation({ summary: 'Listar itens por locação' })
   @ApiQuery({ name: 'locacao_id', required: true, description: 'ID da locação', type: Number })
-  @ApiResponse({ status: 200, description: 'Lista de itens retornada' })
+  @ApiResponse({ status: 200, description: 'Lista de itens da locação retornada' })
   findAll(@Query('locacao_id') locacao_id: number) {
     return this.itemlocacaoService.findAllByLocacao(Number(locacao_id));
   }
@@ -32,16 +32,17 @@ export class ItemlocacaoController {
   }
 
   @Patch(':id')
-  @ApiOperation({ summary: 'Atualizar quantidade do item' })
-  @ApiResponse({ status: 200, description: 'Quantidade atualizada com sucesso' })
+  @ApiOperation({ summary: 'Atualizar quantidade do item de locação' })
+  @ApiResponse({ status: 200, description: 'Quantidade do item de locação atualizada com sucesso' })
   update(@Param('id') id: string, @Body() updateItemlocacaoDto: UpdateItemlocacaoDto) {
     return this.itemlocacaoService.update(+id, updateItemlocacaoDto);
   }
 
   @Delete(':id')
-  @ApiOperation({ summary: 'Remover (soft delete) item da comanda' })
-  @ApiResponse({ status: 200, description: 'Item removido com sucesso (soft delete)' })
+  @ApiOperation({ summary: 'Remover (soft delete) item de locação' })
+  @ApiResponse({ status: 200, description: 'Item de locação removido com sucesso (soft delete)' })
   remove(@Param('id') id: string) {
     return this.itemlocacaoService.remove(+id);
   }
 }
+*** End Patch ***!
